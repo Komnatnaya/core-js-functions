9d412ab705dbbfa3d38ff05eaa436d64ef0da692
Katerina <comina.ru@gmail.com>
2026-01-22 01:35:11 +0200

    feat: add all passing

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -183,8 +183,27 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function fn(...args) {
+    const argsString = args
+      .map((arg) => {
+        return Array.isArray(arg)
+          ? `[${arg
+              .map((argItem) => {
+                return typeof argItem === 'string' ? `"${argItem}"` : argItem;
+              })
+              .join(',')}]`
+          : arg.toString();
+      })
+      .join(',');
+    const funcName = func.name;
+
+    logFunc(`${funcName}(${argsString}) starts`);
+    const result = func(...args);
+    logFunc(`${funcName}(${argsString}) ends`);
+
+    return result;
+  };
 }
 
 /**
